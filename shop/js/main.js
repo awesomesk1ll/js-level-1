296dcb2cf34ceb30165fb0f9cc17dd102a28fddc
Aleksey Skhomenko <a@skhom.ru>
2020-02-09 17:13:00 +0700

    remote content acquiring
    now catalog filling from hosted json file

--- shop/js/main.js
+++ shop/js/main.js
@@ -1,22 +1,45 @@
-let PRODUCTS_NAMES = ['Warcraft 3: Reforged', 'Grand Theft Auto 5', 'Half-Life 2',
-    'StarCraft 2', 'Diablo 2', 'FlatOut 2', 'Counter-Strike', 'Total War: Warhammer'
-];
-let IMGS = ['https://i.playground.ru/e/YNCtBoZigQh3r0BdVeBMLg.jpeg', 'https://i.playground.ru/e/Tf0mwt9jPRX8OLPMsWupgQ.jpeg',
-    'https://i.playground.ru/e/T4l7p0eVZeOBAFQOy8BIpQ.jpeg', 'https://i.playground.ru/e/KloMCCgISWymrCtkSEUHdA.jpeg',
-    'https://i.playground.ru/e/kLs-qQnqWHLdhb0D1C4SYQ.jpeg', 'https://i.playground.ru/e/HxMV0wHI5hayJtUt1teXfA.jpeg',
-    'https://i.playground.ru/e/u18h2k2WwaVr3NNeYt21Gw.jpeg', 'https://i.playground.ru/e/LX-A7f-m02jn2CtRwveB_w.jpeg'
-];
-let PRICES = [25, 20, 10, 20, 5, 5, 5, 15];
-let IDS = [0, 1, 2, 3, 4, 5, 6, 7];
+let catalog = {
+    items: [],
+    container: '.products',
+    cart: null,
+    json_path: 'https://raw.githubusercontent.com/awesomesk1ll/js-level-1/develop/shop/json/response.json',
 
-function createProduct(index) {
-    return {
-        product_name: PRODUCTS_NAMES[index],
-        price: PRICES[index],
-        product_id: IDS[index],
-        img: IMGS[index],
-        quantity: 0,
-        catalog_template() {
+    init() {
+        this.items = [];
+        this.cart = cart;
+
+        this.fetchData(this.json_path)
+            .finally(() => {
+                this._extendData();
+                this._render();
+            })
+
+        document.querySelector(this.container).addEventListener('click', (evt) => {
+            if (evt.target.name == 'buy') {
+                let product = evt.target.dataset;
+                let item = this.items.find(x => x.product_id === product.id);
+                if (this.cart.items.indexOf(item) < 0) this.cart.items.push(item);
+                item.quantity++;
+                this.cart._render();
+                console.log(`${product.name} добавлен в корзину.`);
+            }
+        })
+    },
+    fetchData(url) {
+        return fetch(url)
+            .then(data => data.json())
+            .then((data2) => {
+                this.items = data2
+            })
+    },
+    _extendData() {
+        this.items.forEach(item => {
+            this.extendProduct(item);
+        })
+    },
+    extendProduct(obj) {
+        obj.quantity = 0;
+        obj.catalog_template = function() {
             return `
                         <div class="product-item" data-id="${this.product_id}">
                             <img src="${this.img+'?225x311'}" alt="${this.product_name}">
@@ -32,8 +55,8 @@ function createProduct(index) {
                             </div>
                         </div>
                     `
-        },
-        cart_template() {
+        };
+        obj.cart_template = function() {
             return `
                         <div class="cart-item" data-id="${this.product_id}">
                             <img src="${this.img+'?69x93'}" alt="${this.product_name}">
@@ -42,7 +65,7 @@ function createProduct(index) {
                                 <p class="product-price right-block"> ${this.price}$</p>
 								<div class="quantity">
 									<button name="count-modifier" class="btn" data-id="${this.product_id}" data-modify="-1">-</button>
-									<input name="quantity" type="text" onclick="event.target.select()" data-id="${this.product_id}" value="${this.quantity}">
+									<input name="quantity" type="number" onclick="event.target.select()" data-id="${this.product_id}" value="${this.quantity}">
 									<button name="count-modifier" class="btn" data-id="${this.product_id}" data-modify="+1">+</button>
 								</div><br>
 								
@@ -50,12 +73,20 @@ function createProduct(index) {
 								data-id="${this.product_id}"
 								data-name="${this.product_name}"
 								data-price="${this.price}">
-								X
 							</button>
 							</div>
 						</div>
-                    `
-        }
+                    ` 
+        };
+    },
+    _render() {
+        let container = document.querySelector(this.container);
+        let domString = '';
+
+        this.items.forEach(item => {
+            domString += item.catalog_template();
+        })
+        container.innerHTML = domString;
     }
 };
 
@@ -63,30 +94,31 @@ let cart = {
     items: [],
     container: '.cart-block',
     btn: '.btn-cart',
+
     init() {
-		this._render();
+        this._render();
         document.querySelector(this.btn).addEventListener('click', () => {
             document.querySelector(this.container).classList.toggle('invisible');
         });
         document.querySelector(this.container).addEventListener('change', (evt) => {
-            if (evt.target.name == "remove" || evt.target.name == "count-modifier"|| evt.target.name == "quantity") {
+            if (evt.target.name == "remove" || evt.target.name == "count-modifier" || evt.target.name == "quantity") {
                 let ds = evt.target.dataset;
-                let item = this.items.find(x => x.product_id === +ds.id);
+                let item = this.items.find(x => x.product_id === ds.id);
                 if (evt.target.name == 'quantity') {
                     item.quantity = +evt.target.value;
-                    console.log(item.quantity == 0 ? `${item.product_name} удалён из корзины.` 
-					: `Количество ${item.product_name} теперь равно ${item.quantity}.`);
+                    console.log(item.quantity == 0 ? `${item.product_name} удалён из корзины.` :
+                        `Количество ${item.product_name} теперь равно ${item.quantity}.`);
                 }
-				
+
                 if (item.quantity == 0) this.items.splice(this.items.indexOf(item), 1);
-					this._render();
+                this._render();
             }
             if (evt.target.name == "wipe") this.wipe();
         })
-		document.querySelector(this.container).addEventListener('click', (evt) => {
-            if (evt.target.name == "remove" || evt.target.name == "count-modifier"|| evt.target.name == "quantity") {
+        document.querySelector(this.container).addEventListener('click', (evt) => {
+            if (evt.target.name == "remove" || evt.target.name == "count-modifier" || evt.target.name == "quantity") {
                 let ds = evt.target.dataset;
-                let item = this.items.find(x => x.product_id === +ds.id);
+                let item = this.items.find(x => x.product_id === ds.id);
                 if (evt.target.name == 'remove') {
                     item.quantity = 0;
                     this._render();
@@ -94,16 +126,16 @@ let cart = {
                 }
                 if (evt.target.name == 'count-modifier') {
                     item.quantity += +ds.modify;
-                    console.log(item.quantity == 0 ? `${item.product_name} удалён из корзины.` 
-					: `Количество ${item.product_name} ${ds.modify == "+1" ? "увеличено" : "уменьшено"} и теперь равно ${item.quantity}.`);
+                    console.log(item.quantity == 0 ? `${item.product_name} удалён из корзины.` :
+                        `Количество ${item.product_name} ${ds.modify == "+1" ? "увеличено" : "уменьшено"} и теперь равно ${item.quantity}.`);
                 }
-				
+
                 if (item.quantity == 0) this.items.splice(this.items.indexOf(item), 1);
-				
-				if (evt.target.name == 'quantity') 
-					evt.target.select();
-				else 
-					this._render();
+
+                if (evt.target.name == 'quantity')
+                    evt.target.select();
+                else
+                    this._render();
             }
             if (evt.target.name == "wipe") this.wipe();
         })
@@ -117,8 +149,8 @@ let cart = {
     _do_wipe(items) {
         return items.reduce((a, b) => b["quantity"] = 0, 0)
     },
-	_prepare(domstroke) {
-		if (this.items.length == 0)
+    _prepare(domstroke) {
+        if (this.items.length == 0)
             return '<p>Корзина пуста.</p>';
         else
             return domstroke += `<hr>
@@ -126,7 +158,7 @@ let cart = {
 						Общая стоимость: ${this._get_price(this.items)}$
 						<button name="wipe" class="btn">Очистить корзину</button>
 						`; //костыльчик
-	},
+    },
     _render() {
         let container = document.querySelector(this.container);
         let domString = '';
@@ -135,7 +167,7 @@ let cart = {
             domString += item.cart_template();
         });
 
-		container.innerHTML = this._prepare(domString);
+        container.innerHTML = this._prepare(domString);
     },
     wipe() {
         this._do_wipe(this.items)
@@ -145,41 +177,5 @@ let cart = {
     }
 };
 
-let catalog = {
-    items: [],
-    container: '.products',
-
-    init() {
-        this._fetchItems();
-        this._render();
-
-        document.querySelector(this.container).addEventListener('click', (evt) => {
-            if (evt.target.name == 'buy') {
-                let product = evt.target.dataset;
-                let item = this.items.find(x => x.product_id === +product.id);
-                if (cart.items.indexOf(item) < 0) cart.items.push(item);
-                item.quantity++;
-                cart._render();
-                console.log(`${product.name} добавлен в корзину.`);
-            }
-        })
-    },
-    _fetchItems() {
-        let length = IDS.length;
-
-        for (let i = 0; i < length; i++) {
-            this.items.push(createProduct(i));
-        }
-    },
-    _render() {
-        let container = document.querySelector(this.container);
-        let domString = '';
-
-        this.items.forEach(item => {
-            domString += item.catalog_template();
-        })
-        container.innerHTML = domString;
-    }
-};
 cart.init();
 catalog.init();
